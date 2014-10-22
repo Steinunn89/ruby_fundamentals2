73adcdd7bb91a99793ab57b4ffd8e1780de2cc0d
Steinunn <steinunn.olason@gmail.com>
2014-10-22 14:47:05 -0400

    Completed exercise 7 - used method to amend array with increased students, rounded and added a incremental variable to calculate total.

--- exercise5.rb
+++ exercise5.rb
@@ -1,4 +1,7 @@
 puts "What is the temperature in F?"
+
 temperature_in_F = gets.chomp.to_i
+
 temperature_in_C = (temperature_in_F - 32) * 5/9
+
 puts "Therefore the temperature in Celsius #{temperature_in_C }."
--- exercise7.rb
+++ exercise7.rb
@@ -3,14 +3,51 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22
 }
-students[:cohort4] = 43
-
-def keys (key,value)
-	key.each do |cohort|
-		value.each |num|
-		puts "In " + cohort + "there were" + num + "of students."
+#create method to display hash
+def keys (input)
+	input.each do |cohort, num|
+	puts  "#{cohort} : #{num}" 
 	end
 	
 end
 
+#add to hash
+students[:cohort4] = 43
+
+#call method
 keys(students)
+
+puts " "
+#iterates hash values to increase them by 5%
+def increased_class (inputs)
+	inputs.each do |classes, number|
+		inputs[classes] = (number * 1.05).round
+
+		puts "#{classes} + #{inputs[classes]}"
+	end
+
+end
+
+increased_class(students)
+
+puts " "
+
+students.delete(:cohort2)
+
+keys(students)
+
+puts " "
+
+def total(list)
+	total_students = 0
+	list.map do |cohorts, size|
+		total_students += size
+	end
+	return total_students
+end
+
+
+puts "The total number of students is: #{total(students)}."
+
+
+
